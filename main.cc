f4b94982a6ed5800986f6ae3dbde2c7e62ea7692
bhushanap <baap2499@gmail.com>
2023-07-27 00:35:52 -0400

    added vec3 class

--- main.cc
+++ main.cc
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main()
@@ -9,16 +12,19 @@ int main()
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j<image_height; ++j){
+        std::cerr << "\rScanlines done: " << j << ' ' << std::flush;
         for (int i=0; i<image_width; ++i){
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.25;
+            // auto r = double(i) / (image_width-1);
+            // auto g = double(j) / (image_height-1);
+            // auto b = 0.25;
 
-            int ir = static_cast<int>(255*r);
-            int ig = static_cast<int>(255*g);
-            int ib = static_cast<int>(255*b);
+            // int ir = static_cast<int>(255*r);
+            // int ig = static_cast<int>(255*g);
+            // int ib = static_cast<int>(255*b);
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            // std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
+            write_color(std::cout, pixel_color);        
         }
     }
 }
