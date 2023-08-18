bdb84308f5e65a604f0a8e4dab57fe1ec11bb503
bhushan <83635464+bhushanap@users.noreply.github.com>
2023-08-17 22:51:46 -0400

    Update hittable.h

--- hittable.h
+++ hittable.h
@@ -17,4 +17,20 @@ class hittable {
     virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
 };
 
+class sphere : public hittable {
+  public:
+    ...
+    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const {
+        ...
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+
+        return true;
+    }
+    ...
+};
+
 #endif
