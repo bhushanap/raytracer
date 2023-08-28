09ade2ec5c97927c2b66784630a499a45769d8c0
bhushanap <baap2499@gmail.com>
2023-08-27 23:40:12 -0400

    fixed some issues

--- hittable.h
+++ hittable.h
@@ -8,6 +8,12 @@ class hit_record {
     point3 p;
     vec3 normal;
     double t;
+    bool front_face;
+
+    void set_face_normal(const ray& r, const vec3& outward_normal) {
+      front_face = dot(r.direction(), outward_normal) < 0;
+      normal = front_face ? outward_normal : - outward_normal;
+    }
 };
 
 class hittable {
@@ -15,22 +21,6 @@ class hittable {
     virtual ~hittable() = default;
 
     virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
-};
-
-class sphere : public hittable {
-  public:
-    ...
-    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const {
-        ...
-
-        rec.t = root;
-        rec.p = r.at(rec.t);
-        vec3 outward_normal = (rec.p - center) / radius;
-        rec.set_face_normal(r, outward_normal);
-
-        return true;
-    }
-    ...
 };
 
 #endif
