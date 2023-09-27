971ce50ac5b0541c43b7223fe06e6f984d018215
bhushanap <baap2499@gmail.com>
2023-09-26 21:36:25 -0400

    tried various aa sampling rates

--- rtweekend.h
+++ rtweekend.h
@@ -2,6 +2,7 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
 
@@ -15,6 +16,16 @@ const double pi = 3.14152926;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180;
+}
+
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
 }
 
 #include "interval.h"
